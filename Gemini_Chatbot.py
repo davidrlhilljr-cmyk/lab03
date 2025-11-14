2a793cb032d38a0f7372786086c6ce43ff7bfefd
davidrlhilljr-cmyk <davidrlhilljr@gmail.com>
2025-11-14 17:36:08 -0500

    Delete Gemini_Chatbot.py

--- Gemini_Chatbot.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Gemini Chatbot")
-st.write("This page integrates Google Gemini to create a chatbot that specializes in answering questions based on the API's domain.")
